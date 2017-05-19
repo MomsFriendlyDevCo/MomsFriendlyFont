660e4ccb7f0ceada90c8547807d38fe7a40e0386
Matt Carter <m@ttcarter.com>
2017-05-19 10:19:46 +1000

    BUGFIX: Piping issue with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,17 @@ gulp.task('build', function(cb) {
 	runSequence('fontcustom', 'correctPaths', cb)
 });
 
-gulp.task('fontcustom', function() {
+gulp.task('fontcustom', ()=>
 	gulp.src('./svg')
 		.pipe(fontcustom({
 			font_name: 'moms-friendly-font',
 			'css-selector': '.mf-{{glyph}}'
 		}))
 		.pipe(gulp.dest('./dist'))
-});
+);
 
-gulp.task('correctPaths', function() {
+gulp.task('correctPaths', ()=>
 	gulp.src(['./dist/*.css', './dist/*.scss'])
 		.pipe(replace('./moms-friendly-font', '/fonts/moms-friendly-font'))
 		.pipe(gulp.dest('./dist'))
-});
+);
